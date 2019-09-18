aeab75427f749d5ba7594518f132f5c04de6e80c
MaheshTechnicals <msvarma10001@gmail.com>
2019-09-18 19:13:24 +0000

    device tree for oxygen (pie)

---
+++ msm8953-common/vendor_prop.mk
@@ -0,0 +1,249 @@
+#
+# system.prop for msm8953-common
+#
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+  af.fast_track_multiplier=2 \
+  audio.deep_buffer.media=true \
+  audio.offload.disable=true \
+  audio.offload.min.duration.secs=30 \
+  audio.offload.video=true \
+  persist.vendor.audio.fluence.speaker=true \
+  persist.vendor.audio.fluence.voicecall=true \
+  persist.vendor.audio.fluence.voicerec=true \
+  persist.vendor.btstack.enable.splita2dp=false \
+  persist.vendor.audio.hw.binder.size_kbyte=1024 \
+  ro.vendor.audio.sdk.fluencetype=fluence \
+  ro.af.client_heap_size_kbyte=7168 \
+  ro.vendor.audio.sdk.ssr=false \
+  vendor.audio.flac.sw.decoder.24bit=true \
+  vendor.audio.offload.buffer.size.kb=64 \
+  vendor.audio.offload.gapless.enabled=true \
+  vendor.audio.offload.multiaac.enable=true \
+  vendor.audio.offload.multiple.enabled=false \
+  vendor.audio.offload.track.enable=true \
+  vendor.audio.parser.ip.buffer.size=0 \
+  vendor.audio.playback.mch.downsample=true \
+  vendor.audio.pp.asphere.enabled=false \
+  vendor.audio.safx.pbe.enabled=true \
+  vendor.audio.tunnel.encode=false \
+  vendor.audio.use.sw.alac.decoder=true \
+  vendor.audio.use.sw.ape.decoder=true \
+  vendor.audio_hal.period_size=192 \
+  vendor.voice.conc.fallbackpath=deep-buffer \
+  audio.dolby.ds2.hardbypass=true \
+  vendor.audio.dolby.ds2.enabled=true \
+  vendor.voice.path.for.pcm.voip=true \
+  vendor.voice.playback.conc.disabled=true \
+  vendor.voice.record.conc.disabled=false \
+  ro.config.media_vol_steps=25 \
+  ro.config.vc_call_vol_steps=7 \
+  persist.audio.dirac.speaker=true \
+  vendor.voice.voip.conc.disabled=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+  bluetooth.hfp.client=1 \
+  qcom.bluetooth.soc=smd \
+  ro.bluetooth.hfp.ver=1.7 \
+  ro.qualcomm.bt.hci_transport=smd
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+  camera.display.lmax=1280x720 \
+  camera.display.umax=1920x1080 \
+  camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
+  camera.lowpower.record.enable=1 \
+  media.camera.ts.monotonic=1 \
+  persist.camera.gyro.disable=0 \
+  persist.camera.isp.clock.optmz=0 \
+  persist.camera.stats.test=5 \
+  persist.camera.eis.enable=1 \
+  persist.vendor.qti.telephony.vt_cam_interface=1 \
+  vidc.enc.dcvs.extra-buff-count=2 \
+  camera.hal1.packagelist=com.whatsapp \
+  persist.camera.HAL3.enabled=1
+
+
+#disable UBWC for camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.preview.ubwc=0 \
+    persist.camera.stats.test=0 \
+    persist.camera.depth.focus.cb=0 \
+    persist.camera.isp.clock.optmz=0 \
+    persist.camera.hist.high=20 \
+    persist.camera.hist.drc=1.2 \
+    persist.camera.linkpreview=0 \
+    persist.camera.isp.turbo=1
+
+# Cne/Dpm
+PRODUCT_PROPERTY_OVERRIDES += \
+  persist.cne.feature=1 \
+  persist.dpm.feature=1
+
+# Coresight
+  PRODUCT_PROPERTY_OVERRIDES += \
+  persist.debug.coresight.config=stm-events
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+ debug.egl.hw=0 \
+ debug.enable.sglscale=1 \
+ debug.gralloc.enable_fb_ubwc=1 \
+ debug.mdpcomp.logs=0 \
+ debug.sf.hw=0 \
+ dev.pm.dyn_samplingrate=1 \
+ persist.debug.wfd.enable=1 \
+ persist.demo.hdmirotationlock=false \
+ vendor.display.disable_rotator_downscale=1 \
+ persist.hwc.enable_vds=1 \
+ persist.hwc.mdpcomp.enable=true \
+ ro.opengles.version=196610 \
+ ro.qualcomm.cabl=0 \
+ ro.vendor.display.cabl=2 \
+ ro.sf.lcd_density=320 \
+ persist.sys.wfd.nohdcp=1 \
+ persist.debug.wfd.enable=1 \
+ persist.sys.wfd.virtual=0 
+
+# Fp Gestures
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.fp.navigation=1
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+ drm.service.enabled=true
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.qfp=false \
+ ro.fingerprint.cleanup.unused=false
+
+# Fm
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.fm.transmitter=false
+
+# Frp
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.frp.pst=/dev/block/bootdevice/by-name/config
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.gps.qc_nlp_in_use=1 \
+ persist.loc.nlp_name=com.qualcomm.location \
+ ro.gps.agps_provider=1
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+ av.debug.disable.pers.cache=1 \
+ media.aac_51_output_enabled=true \
+ media.msm8956hw=0 \
+ media.stagefright.audio.sink=280 \
+ mm.enable.qcom_parser=1048575 \
+ mm.enable.sec.smoothstreaming=true \
+ mmp.enable.3g2=true \
+ vendor.audio.hw.aac.encoder=true \
+ vendor.vidc.dec.downscalar_height=1088 \
+ vendor.vidc.dec.downscalar_width=1920 \
+ vendor.vidc.disable.split.mode=1 \
+ vendor.vidc.enc.disable.pq=true \
+ vendor.vidc.enc.disable_bframes=1 \
+ debug.sf.enable_hwc_vds=1 \
+ persist.sys.wfd.virtual=0
+
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.sys.fw.dex2oat_thread_count=4 \
+ ro.vendor.extension_library=libqti-perfd-client.so
+
+# Netmgrd
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.use_data_netmgrd=true \
+ persist.data.netmgrd.qos.enable=true \
+ persist.data.mode=concurrent
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.rild.nitz_plmn="" \
+ persist.rild.nitz_long_ons_0="" \
+ persist.rild.nitz_long_ons_1="" \
+ persist.rild.nitz_long_ons_2="" \
+ persist.rild.nitz_long_ons_3="" \
+ persist.rild.nitz_short_ons_0="" \
+ persist.rild.nitz_short_ons_1="" \
+ persist.rild.nitz_short_ons_2="" \
+ persist.rild.nitz_short_ons_3=""
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+ DEVICE_PROVISIONED=1 \
+ persist.data.iwlan.enable=true \
+ persist.dbg.ims_volte_enable=1 \
+ persist.dbg.volte_avail_ovr=1 \
+ persist.dbg.vt_avail_ovr=1 \
+ persist.dbg.wfc_avail_ovr=0 \
+ persist.vendor.radio.apm_sim_not_pwdn=1 \
+ persist.radio.calls.on.ims=0 \
+ persist.radio.csvt.enabled=false \
+ persist.vendor.radio.hw_mbn_update=0 \
+ persist.radio.jbims=0 \
+ persist.radio.mt_sms_ack=20 \
+ persist.radio.multisim.config=dsds \
+ persist.vendor.sw_mbn_update=0 \
+ persist.radio.videopause.mode=1 \
+ persist.vendor.radio.custom_ecc=1 \
+ persist.vendor.radio.rat_on=combine \
+ persist.vendor.radio.sib16_support=1 \
+ ril.subscription.types=NV,RUIM \
+ rild.libargs=-d/dev/smd0 \
+ rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+ vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+ ro.telephony.call_ring.multiple=false \
+ ro.telephony.default_network=22,22 \
+ service.qti.ims.enabled=1 \
+ telephony.lteOnCdmaDevice=1
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.timed.enable=true
+
+# Tcp
+PRODUCT_PROPERTY_OVERRIDES += \
+ net.tcp.2g_init_rwnd=10
+
+# Usb
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.sys.usb.config.extra=none
+    
+  
+   # IMS / VoLTE
+PRODUCT_PROPERTY_OVERRIDES += \
+ persist.dbg.volte_avail_ovr=1 \
+ persist.dbg.vt_avail_ovr=1 \
+ persist.dbg.wfc_avail_ovr=1 \
+ persist.radio.lte_vrte_ltd=1 \
+ persist.radio.VT_CAM_INTERFACE=2 \
+ persist.radio.VT_ENABLE=1 \
+ persist.radio.VT_HYBRID_ENABLE=1 \
+ persist.volte_enabled_by_hw=1 
+
+
+
+
+# Optimize
+ PRODUCT_PROPERTY_OVERRIDES += \
+     sys.use_fifo_ui=1
+
+# DPM
+#PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=5
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+ wifi.interface=wlan0
+
+#low audio flinger standby delay to reduce power consumption
+  PRODUCT_PROPERTY_OVERRIDES += \
+  ro.audio.flinger_standbytime_ms=300
---
+++ oxygen/device.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, vendor/xiaomi/oxygen/oxygen-vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
+# Inherit from msm8953-common
+$(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_oxygen
+
+# Fingerprint
+#PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
+
+
+# Consumerir
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-service.xiaomi_oxygen
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/ft5x46.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5x46.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsx.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsx.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_dsxv26.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/synaptics_dsxv26.kl \
+    $(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.oxygen.rc \
+    init.goodix.sh
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.xiaomi_oxygen
+
+# Vndk-hack
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:system/lib64/android.hardware.gnss@1.0-v27.so
+
+# Google Lens
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017_exclusive.xml:system/etc/sysconfig/pixel_2017_exclusive.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/pixel_2017.xml:system/etc/sysconfig/pixel_2017.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/nexus.xml:system/etc/sysconfig/nexus.xml \
+    $(LOCAL_PATH)/pixelstyle/etc/sysconfig/google_build.xml:system/etc/sysconfig/google_build.xml
